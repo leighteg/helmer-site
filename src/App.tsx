973a19a8a9bb28f7c772d96643c034b0baa84885
Leighton Tegland <leightontegland7@gmail.com>
2025-09-02 19:15:36 -0400

    visual improvements, added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import SponzaColumnsScreenshot from './assets/screenshots/sponza_columns.png'
 import SponzaRaptorScreenshot from './assets/screenshots/sponza_raptor.png'
+import AppleLogo from './assets/logos/apple_black.svg'
+import WindowsLogo from './assets/logos/windows.svg'
 import Carousel from './components/Carousel/Carousel'
 import './App.css'
 
@@ -8,29 +10,40 @@ const App = () => {
         <>
             <Carousel images={[SponzaColumnsScreenshot, SponzaRaptorScreenshot]} />
 
-            <span style={{ position: 'absolute', padding: '2em', display: 'flex', "flex-direction": 'column', gap: '2em' }}>
-                <div class='info'>
-                    helmer is built on a simple but ambitious principle: balance. Every decision—technical, aesthetic, and practical—strives to equally value performance, visual fidelity, portability, and developer quality of life. The result is a game engine designed to look great, run well, and work on anything.
+            <div style={{ display: 'flex', "justify-content": 'center' }}>
+                <div class='base'>
+                    <div class='info'>
+                        helmer is built on a simple but ambitious principle: balance. Every decision—technical, aesthetic, and practical—strives to equally value performance, visual fidelity, portability, and developer quality of life. The result is a game engine designed to look great, run well, and work on anything.
 
-                    At its core, the engine features a flexible ECS architecture with three purpose-built renderers:
+                        At its core, the engine features a flexible ECS architecture with three purpose-built renderers:
 
-                    Deferred Renderer – A cutting-edge pipeline supporting advanced features like SSGI, SSR, and other modern effects, designed for high-end visuals.
+                        Deferred Renderer – A cutting-edge pipeline supporting advanced features like SSGI, SSR, and other modern effects, designed for high-end visuals.
 
-                    Forward Renderer (Modern) – Optimized for lower-end but still modern hardware, offering efficient per-material uniform handling.
+                        Forward Renderer (Modern) – Optimized for lower-end but still modern hardware, offering efficient per-material uniform handling.
 
-                    Forward Renderer (Legacy) – Built with maximum compatibility in mind, targeting older devices with texture array–based rendering.
+                        Forward Renderer (Legacy) – Built with maximum compatibility in mind, targeting older devices with texture array–based rendering.
 
-                    Currently in alpha and closed source (commercially developed), the engine is under active development with many more features planned. The mission remains constant: a platform where no single characteristic overshadows the rest—where graphics, performance, accessibility, and developer workflow exist in harmony.
+                        Currently in alpha and closed source (commercially developed), the engine is under active development with many more features planned. The mission remains constant: a platform where no single characteristic overshadows the rest—where graphics, performance, accessibility, and developer workflow exist in harmony.
+                    </div>
+
+                    <div class='downloadPane'>
+                        <h3 style={{ margin: '0' }}>sponza demo</h3>
+                        <span>helmer is still under heavy active development, and is not yet ready for production use. but to test helmer, a sponza scene demo is available for download.</span>
+                        <span style={{ display: 'flex', "flex-direction": 'column', gap: '.5em' }}>
+                            <button class='windows'>
+                                <img src={WindowsLogo} height={'90%'} />
+                                Windows (x64)
+                            </button>
+                            <button class='macos'>
+                                <img src={AppleLogo} height={'90%'} />
+                                macOS (Apple Silicon)
+                            </button>
+                        </span>
+                    </div>
                 </div>
+            </div>
 
-                <div class='downloadPane'>
-                    <span>helmer is still under heavy active development, and is not yet ready for production use. but to test helmer, a sponza scene demo is available for download.</span>
-                    <span style={{ display: 'flex', "flex-direction": 'column', gap: '.5em' }}>
-                        <button>Windows (x64)</button>
-                        <button>macOS (Apple Silicon)</button>
-                    </span>
-                </div>
-            </span>
+            <footer>© 2025 leighton</footer>
         </>
     )
 }
